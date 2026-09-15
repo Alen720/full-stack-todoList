162939edbaab4d72cc049c699ab523f61612c80c
Alen720 <alenpro720@gmail.com>
2026-09-16 03:52:16 +0400

    start write frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState([])
+  
+  const initialValue = {
+    userText: ""
+  }
+
+  const API = "http://127.0.0.1:8000"
+  
+  function getTodos() {
+    fetch(`${API}/todos`)
+    .then((res) => res.json())
+    .then((data) => setText(data))
+  }
+
+  useEffect(() => {
+    getTodos()
+  })
+
   return (
     <div className="App">
-      
+      {text.length > 0 && (
+          <div className="content">
+            {text.map((item, index) => (
+                <span key={index}>{item.text}</span>
+            ))}
+          </div>
+        )}
     </div>
   );
 }

72d0a53105347e753e1cd5d334b913878cd71c06
Alen720 <alenpro720@gmail.com>
2026-09-22 01:42:23 +0400

    fix bugs and finish functional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
+
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState([])
-  
-  const initialValue = {
-    userText: ""
-  }
+  const [text, setText] = useState([]);
 
-  const API = "http://127.0.0.1:8000"
-  
+  const initialValues = {
+    text: "",
+  };
+
+  const API = "http://127.0.0.1:8000";
+
   function getTodos() {
     fetch(`${API}/todos`)
-    .then((res) => res.json())
-    .then((data) => setText(data))
+      .then((res) => res.json())
+      .then((data) => setText(data));
   }
 
   function createTodo(text) {
@@ -23,42 +25,70 @@ function App() {
         "Content-type": "application/json",
       },
       body: JSON.stringify({
-        text: text
-      })
-    }).then(() => getTodos())
+        text: text,
+      }),
+    }).then(() => getTodos());
   }
 
   function toggleCheck(id, checked) {
     fetch(`${API}/todos/${id}`, {
       method: "PUT",
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
       body: JSON.stringify({
-        "Checked": checked
-      })
-    }).then(() => getTodos())
+        checked: checked,
+      }),
+    }).then(() => getTodos());
   }
 
   function deleteTodo(id) {
     fetch(`${API}/todos/${id}`, {
-      method: "DELETE"
-    }).then(() => getTodos())
+      method: "DELETE",
+    }).then(() => getTodos());
   }
 
   useEffect(() => {
-    getTodos()
-  })
+    getTodos();
+  }, []);
 
   return (
     <div className="App">
-      {text.length > 0 && (
+      <div className="content">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(value, { resetForm }) => {
+            createTodo(value.text);
+            resetForm();
+          }}
+        >
+          <Form>
+            <Field
+              name="text"
+              placeholder="What you need to do?"
+              autoComplete="off"
+            ></Field>
+          </Form>
+        </Formik>
+
+        {text.length > 0 && (
           <div className="content">
-            {text.map((item, index) => (
-                <span key={index}>{item.text}</span>
+            {text.map((item) => (
+              <label key={item.id}>
+                <input
+                  type="checkbox"
+                  checked={item.checked}
+                  onChange={(e) => toggleCheck(item.id, e.target.checked)}
+                />
+                <span>{item.text}</span>
+                <button type="button" onClick={() => deleteTodo(item.id)}>
+                  DELETE
+                </button>
+              </label>
             ))}
           </div>
         )}
+      </div>
     </div>
   );
 }
